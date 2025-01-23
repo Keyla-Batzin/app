e35bb35b07d15e9ad9d1230c83025e6ee2c74b2a
Keyla <2023_keyla.batzin@iticbcn.cat>
2025-01-23 16:32:56 +0100

    Programando los primeros botones

--- app/src/main/java/com/example/bloom/Login.kt
+++ app/src/main/java/com/example/bloom/Login.kt
@@ -1,6 +1,9 @@
 package com.example.bloom
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,5 +13,23 @@ class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val btn_login = findViewById<Button>(R.id.btn_login)
+        btn_login.setOnClickListener { // Cambiar a la MainActivity
+            val intent: Intent =
+                Intent(
+                    this@Login,
+                    MainActivity::class.java
+                )
+            startActivity(intent)
+        }
+
+        val textRegistrar = findViewById<TextView>(R.id.textRegistrar)
+
+        textRegistrar.setOnClickListener {
+            // Cambiar a la actividad SingUp
+            val intent = Intent(this@Login, SingUp::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/bloom/SingUp.kt
+++ app/src/main/java/com/example/bloom/SingUp.kt
@@ -1,6 +1,10 @@
 package com.example.bloom
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +15,25 @@ class SingUp : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_sing_up)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btn_login = findViewById<Button>(R.id.btn_login)
+
+        btn_login.setOnClickListener {
+            // Mostrar un Toast antes de cambiar de actividad
+            Toast.makeText(this, "Cuenta creada con éxito", Toast.LENGTH_SHORT).show()
+
+            // Cambiar a la Login al crear la cuenta
+            val intent = Intent(this@SingUp, Login::class.java)
+            startActivity(intent)
         }
+
+        val textLogin = findViewById<TextView>(R.id.textIrALogin)
+
+        textLogin.setOnClickListener {
+            // Cambiar a la actividad Login
+            val intent = Intent(this@SingUp, Login::class.java)
+            startActivity(intent)
+        }
+
     }
 }
