8304d6de1481e3631b19e75811c2fcf14cc77dfd
Keyla <2023_keyla.batzin@iticbcn.cat>
2025-03-19 15:58:57 +0100

    Retoques

--- app/src/main/java/com/example/bloom/Login.kt
+++ app/src/main/java/com/example/bloom/Login.kt
@@ -6,39 +6,27 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import com.example.bloom.databinding.ActivityMainBinding
 import com.example.bloom.pantallahome.ActivityPrincipal
-import com.example.bloom.pantallahome.MainActivity
 
 class Login : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
-        // Configurar SplashScreen
-        val splashScreen = installSplashScreen()
-
-        // Inicializar View Binding
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        splashScreen.setKeepOnScreenCondition { false }
+        // SplashScreen
+        val splash = installSplashScreen()
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val btn_login = findViewById<Button>(R.id.btn_login)
-        btn_login.setOnClickListener { // Cambiar a la MainActivity
-            val intent: Intent =
-                Intent(this@Login, ActivityPrincipal::class.java)
-            startActivity(intent)
+        splash.setKeepOnScreenCondition{ false }
+
+        // Botón de login
+        findViewById<Button>(R.id.btn_login).setOnClickListener {
+            startActivity(Intent(this, ActivityPrincipal::class.java))
         }
 
-        val textRegistrar = findViewById<TextView>(R.id.textRegistrar)
-
-        textRegistrar.setOnClickListener {
-            // Cambiar a la actividad SingUp
-            val intent = Intent(this@Login, SingUp::class.java)
-            startActivity(intent)
+        // Botón de registro
+        findViewById<TextView>(R.id.textRegistrar).setOnClickListener {
+            startActivity(Intent(this, SingUp::class.java))
         }
     }
 }
+
